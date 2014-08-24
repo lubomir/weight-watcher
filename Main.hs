ae81ddac17c6bb3e83e5859655841932368658aa
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2014-08-24 18:43:12 +0200

    Stored data into database

--- Main.hs
+++ Main.hs
@@ -1,82 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Data.Char (isDigit)
-import Data.List.Split
-import Data.List
-import Data.Maybe (fromMaybe)
-import Control.Applicative
-import Control.Monad (mzero, when)
-import Control.Monad.IO.Class (liftIO)
-import Web.Scotty
-import Network.Wai.Middleware.RequestLogger
-import System.Locale (defaultTimeLocale)
-import Data.Time
-import Data.Aeson
-import System.Lock.FLock
-import System.Directory
-import System.Environment
+import           Control.Applicative
+import           Control.Monad                        (mzero)
+import           Control.Monad.IO.Class               (liftIO)
+import           Control.Monad.Logger                 (runNoLoggingT)
+import           Control.Monad.Trans.Resource         (runResourceT)
+import           Data.Aeson
+import           Data.List                            (genericLength)
+import           Data.Text.Format
+import qualified Data.Text.Lazy                       as T
+import           Data.Time
+import           Database.Persist                     hiding (get)
+import           Database.Persist.Sql                 hiding (get)
+import           Database.Persist.Sqlite              hiding (get)
+import           Network.Wai.Middleware.RequestLogger
+import           System.Environment
+import           System.Locale                        (defaultTimeLocale)
+import           Web.Scotty
 
-data Record = Record { date :: UTCTime
-                     , weight :: Float
-                     } deriving (Eq, Show)
+import Model
+--import DBUtils
 
-instance FromJSON Record where
-    parseJSON (Object v) = do
-        datestr <- v .: "date"
-        case parseTime defaultTimeLocale "%Y-%m-%d" datestr of
-            Nothing -> mzero
-            Just d  -> Record d <$> v .: "weight"
+data InputRecord = InputRecord { date :: UTCTime
+                               , weight :: Double
+                               } deriving (Eq, Show)
+
+instance FromJSON InputRecord where
+    parseJSON (Object v) = InputRecord <$> v .: "date" <*> v .: "weight"
     parseJSON _ = mzero
-
-
-dataFile :: FilePath
-dataFile = "weights.csv"
 
 period :: Int
 period = 28
 
-lastN :: Int -> [a] -> [a]
-lastN n xs = let l = length xs in drop (l - n) xs
-
 average :: (Real a, Fractional b) => [a] -> b
 average xs = realToFrac (sum xs) / genericLength xs
 
-extract :: String -> Float
-extract s = case splitOn "," s of
-    [_, x, _] -> read x
-    _ -> 0
+renderDate :: UTCTime -> T.Text
+renderDate = T.pack . formatTime defaultTimeLocale "%Y-%m-%d"
 
-renderDate :: UTCTime -> String
-renderDate = formatTime defaultTimeLocale "%Y-%m-%d"
+mkRecord :: InputRecord -> [Record] -> Record
+mkRecord ir rs = Record (date ir) (weight ir) (average (weight ir : map recordWeight rs))
 
-saveData :: FilePath -> Record -> IO ()
-saveData fp r = withLock fp Exclusive Block worker
+renderRecords :: [Record] -> T.Text
+renderRecords rs = T.concat (names : map conv rs)
   where
-    tmpFile = fp ++ ".out"
-    worker = do
-        dat <- lines <$> readFile fp
-        let prev = (map extract . lastN period . drop 1) dat
-        let newAvg = average (weight r : prev) :: Float
-        let newLine = intercalate "," [ renderDate $ date r
-                                      , show $ weight r
-                                      , show newAvg]
-        writeFile tmpFile $ unlines (dat ++ [newLine])
-        renameFile tmpFile fp
-
-getPort :: IO (Maybe Int)
-getPort = do
-    args <- getArgs
-    case args of
-        [mport] -> return $ if all isDigit mport
-                                then Just $ read mport
-                                else Nothing
-        _ -> return Nothing
+    names = "Date,Weight,Avg\n"
+    conv (Record {..}) = format "{},{},{}\n" ( renderDate recordDate
+                                             , fixed 2 recordWeight
+                                             , fixed 2 recordAvg
+                                             )
 
 main :: IO ()
 main = do
-    port <- fromMaybe 3000 <$> getPort
-    firstRun <- not <$> doesFileExist dataFile
-    when firstRun $ writeFile dataFile "Date,Weight,Avg\n"
+    port <- maybe 3000 read <$> lookupEnv "PORT"
+    pool <- createSqlitePool "db.sqlite" 1
+    runNoLoggingT $ runSqlPool (runMigration migrateAll) pool
+    let runDB = liftIO . runNoLoggingT . runResourceT . flip runSqlPool pool
+
     scotty port $ do
         middleware logStdoutDev
         get "/" $ do
@@ -87,8 +68,11 @@ main = do
             file "dygraph-combined.js"
         get "/data.csv" $ do
             setHeader "Content-Type" "text/plain"
-            file dataFile
+            rs <- runDB $ selectList [] [Asc RecordDate]
+            text $ renderRecords (map entityVal rs)
         post "/" $ do
             input <- jsonData
-            liftIO $ saveData dataFile input
+            _ <- runDB $ do
+                prev <- map entityVal <$> selectList [] [Desc RecordDate, LimitTo period]
+                insert $ mkRecord input prev
             redirect "/"
---
+++ Model.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE EmptyDataDecls       #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Model where
+
+import           Data.Time           (UTCTime)
+import           Database.Persist
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Record json
+    date    UTCTime
+    weight  Double
+    avg     Double
+  deriving Show Read Eq Ord
+|]
