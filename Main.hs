ff5b8a2132ca222c70b5ed7836fa6005b0b09eda
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-05-23 15:30:17 +0200

    Simplify code

--- Main.hs
+++ Main.hs
@@ -1,23 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import           Data.Monoid
-import           Data.Text.Lazy              (Text)
 import qualified Database.Persist            as DB
 import qualified Database.Persist.Postgresql as DB
 import           Network.HTTP.Types.Status   (created201)
-import           Web.Scotty.Trans            (capture, file, get, json,
-                                              jsonData, param, post, setHeader,
-                                              status)
+import           Web.Scotty.Trans            (file, get, json, jsonData, post,
+                                              setHeader, status)
 
 import Model
 import WebService
-
-serveDir :: FilePath -> Text -> Handler
-serveDir dir mime = get (capture $ "/" <> dir <> "/:filename") $ do
-    filename <- param "filename"
-    setHeader "Content-Type" mime
-    file (dir <> "/" <> filename)
 
 main :: IO ()
 main = runService $ do
@@ -25,8 +16,9 @@ main = runService $ do
     get "/" $ do
         setHeader "Content-Type" "text/html"
         file "index.html"
-    serveDir "js" "text/javascript"
-    serveDir "css" "text/css"
+    get "/js/dygraph-combined.js" $ do
+        setHeader "Content-Type" "text/javascript"
+        file "js/dygraph-combined.js"
     get "/data.json" $ do
         rs <- runDB $ DB.selectList [] [DB.Asc RecordDate]
         json rs
