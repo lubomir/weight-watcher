7d705cdd52b82d49345ade840e0ba8db2f29dd9f
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-09-01 21:58:40 +0200

    Use only dates without times

--- src/Model.hs
+++ src/Model.hs
@@ -7,15 +7,28 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Model where
 
-import           Data.Time           (UTCTime)
+import           Control.Monad (mzero)
+import           Data.Aeson
+import           Data.Text     (pack, unpack)
+import           Data.Time     (Day, parseTime)
+import           System.Locale (defaultTimeLocale)
+
 import           Database.Persist.TH
+
+instance ToJSON Day where
+    toJSON       = String . pack . show
+
+instance FromJSON Day where
+    parseJSON = withText "Day" $
+        maybe mzero return . parseTime defaultTimeLocale "%Y-%m-%d" . unpack
 
 share [mkMigrate "migrateAll", mkPersist sqlSettings] [persistLowerCase|
 Record json
-    date    UTCTime
+    date    Day
     weight  Double
     deriving Show
 |]
